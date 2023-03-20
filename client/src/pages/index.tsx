e313accf51771d16322f420338fb7191f89ec37e
arvin018 <arvin.aditama@gmail.com>
2023-03-20 12:27:31 +0700

    routing-next-js

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Link from  'next/link'
+
+export default function Home() {
+  return (
+    <>
+    <header>
+
+      <ul>
+        <li> <Link href="/" > Home </Link></li>
+        <li> <Link href="/blog" > Blog </Link></li>
+        <li> <Link href="/users" > Users </Link></li>
+    
+      </ul>
+    </header>
+     <h1>Welcome Wantau</h1>
+    </>
+  )
+}
